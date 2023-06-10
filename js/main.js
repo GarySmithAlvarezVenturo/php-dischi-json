e3a06dc858851cdfef8932e825a3abc32a4b3151
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-06-10 16:21:46 +0200

    add GETaxios

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+           
+            vinylLists: [],
+        }
+    },
+    created(){
+        axios.get('http://localhost/php-dischi-json/api.php', {
+            params: {
+            }
+        })
+        .then(res => {
+            this.vinylLists = res.data;
+        });
+    }
+}).mount('#app')
